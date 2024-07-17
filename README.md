869c62e225a7e0de3a254321c05ff6826df5a658
Piotr Tutak <piotr.tutak@getindata.com>
2024-07-17 14:47:41 +0200

    update models

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # cor-iesu
 
+## run
+
 ```bash
 flask --app 'cor_iesu.app:create_app("./config/dev-config.ini")' run
+```
+
+
+## migrate
+
+```bash
+flask --app 'cor_iesu.app:create_app("./config/dev-config.ini")' db migrate
+flask --app 'cor_iesu.app:create_app("./config/dev-config.ini")' db upgrade
 ```
--- src/cor_iesu/models.py
+++ src/cor_iesu/models.py
@@ -8,7 +8,7 @@ migrate = Migrate()
 
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
@@ -16,15 +16,16 @@ class User(db.Model):
     phone_number: Mapped[str] = mapped_column(db.String(9), unique=True, nullable=False)
     first_name: Mapped[str] = mapped_column(String(80), nullable=False)
     last_name: Mapped[str] = mapped_column(String(80), nullable=False)
-    admin: Mapped["Admin"] = relationship(back_populates="user")
+    admin: Mapped["Admin"] = relationship(back_populates="users")
+    period_assignments: Mapped[list["PeriodAssignment"]] = relationship(back_populates="users")
 
 
 class Admin(db.Model):
-    __tablename__ = "admin"
+    __tablename__ = "admins"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    id_user: Mapped[int] = mapped_column(ForeignKey("user.id"))
-    user: Mapped["User"] = relationship(back_populates="admin")
+    id_user: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="admins")
 
 
 class Config(db.Model):
@@ -34,3 +35,48 @@ class Config(db.Model):
     name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     value: Mapped[str] = mapped_column(String(255), nullable=False)
     description: Mapped[str] = mapped_column(String(600), nullable=False)
+
+
+class Period(db.Model):
+    __tablename__ = "periods"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(600), nullable=False)
+
+
+class Collection(db.Model):
+    __tablename__ = "collections"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(600), nullable=False)
+    config: Mapped["CollectionConfig"] = relationship(back_populates="collections", uselist=False)
+    periods: Mapped[list["PeriodCollection"]] = relationship(back_populates="collections")
+
+class PeriodCollection(db.Model):
+    __tablename__ = "period_collections"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_period: Mapped[int] = mapped_column(ForeignKey("periods.id"))
+    id_collection: Mapped[int] = mapped_column(ForeignKey("collections.id"))
+    collection: Mapped["Collection"] = relationship(back_populates="period_collections")
+    period: Mapped["Period"] = relationship(back_populates="period_collections")
+
+
+class CollectionConfig(db.Model):
+    __tablename__ = "collection_configs"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_collection: Mapped[int] = mapped_column(ForeignKey("collections.id"))
+    collection: Mapped["Collection"] = relationship(back_populates="configs")
+    config: Mapped["Config"] = relationship(back_populates="collections")
+
+
+class PeriodAssignment(db.Model):
+    __tablename__ = "period_assignments"
+
+    id_period_collection: Mapped[int] = mapped_column(ForeignKey("period_collections.id"), primary_key=True)
+    id_user: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    period_collection: Mapped["PeriodCollection"] = relationship(back_populates="period_assignments")
+    user: Mapped["User"] = relationship(back_populates="period_assignments")
